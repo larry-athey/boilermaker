a8c1d353b39e998a9b10ca364354b07954dbde3a
Larry Athey <panhandleponics@larryathey.com>
2024-12-12 12:28:15 -0700

    Update web_ui.h

--- boiler_brains/web_ui.h
+++ boiler_brains/web_ui.h
@@ -1,5 +1,25 @@
 //------------------------------------------------------------------------------------------------
 // Boilermaker SCR/SSR Brain | (CopyLeft) 2024-Present | Larry Athey (https://panhandleponics.com)
 //------------------------------------------------------------------------------------------------
+void PingSlaves() {
 
+}
+//------------------------------------------------------------------------------------------------
+void UpdateSlave(byte WhichOne,String Query) {
+  HTTPClient http;
+  http.begin("http://example.com/json-endpoint");  // Replace with your JSON endpoint
+    
+  // Send HTTP GET request
+  int httpResponseCode = http.GET();
+    
+  if (httpResponseCode > 0) {
+    String payload = http.getString();
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println(payload);  // This will print the JSON string
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();  // Close connection
+}
 //------------------------------------------------------------------------------------------------
