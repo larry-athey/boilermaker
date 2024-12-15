a99a39b586de5c32074302c162715fb790fb1aae
Larry Athey <121518798+larry-athey@users.noreply.github.com>
2024-12-15 12:32:50 -0700

    Update slave_sync.h

--- boiler_brains/slave_sync.h
+++ boiler_brains/slave_sync.h
@@ -10,19 +10,19 @@ byte SlaveTotal() { // Returns the total number of configured slave units
   return Total;
 }
 //------------------------------------------------------------------------------------------------
-String GetSlaveIP(byte WhichOne) {
+String GetSlaveIP(byte WhichOne) { // Returns the IP address of a slave (1..4) as a String type
   if (WhichOne == 1) return slaveIP1;
   if (WhichOne == 2) return slaveIP2;
   if (WhichOne == 3) return slaveIP3;
   if (WhichOne == 4) return slaveIP4;
 }
 //------------------------------------------------------------------------------------------------
-bool PingSlave(byte WhichOne) {
+bool PingSlave(byte WhichOne) { // Returns true if a slave unit (1..4) is alive and pinging
   bool PingTest = Ping.ping(GetSlaveIP(WhichOne).c_str(),3);
   return PingTest;
 }
 //------------------------------------------------------------------------------------------------
-byte PingAllSlaves() {
+byte PingAllSlaves() { // Pings all slaves and returns the number of how many are pinging
   byte Total = 0;
   if (SlaveTotal() == 0) return 0;
   if (Ping.ping(GetSlaveIP(1).c_str(),3)) Total ++;
@@ -32,7 +32,7 @@ byte PingAllSlaves() {
   return Total;
 }
 //------------------------------------------------------------------------------------------------
-void UpdateSlave(byte WhichOne,String Query) {
+void UpdateSlave(byte WhichOne,String Query) { // Update a slave unit with an HTTP API call
   /*
   HTTPClient http;
   http.begin("http://example.com/json-endpoint");  // Replace with your JSON endpoint
@@ -52,7 +52,7 @@ void UpdateSlave(byte WhichOne,String Query) {
   */
 }
 //------------------------------------------------------------------------------------------------
-void UpdateAllSlaves(String Query) {
+void UpdateAllSlaves(String Query) { // Updates all slaves with the same HTTP API call
   if (SlaveTotal() > 0) {
     for (byte x = 1; x <= 4; x ++) {
       if (PingSlave(x)) UpdateSlave(x,Query);
