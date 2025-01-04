cfaedfc2144986da8a91e24bc98a33130518358f
Larry Athey <panhandleponics@larryathey.com>
2025-01-03 23:58:03 -0700

    Update boiler_brains.ino

--- boiler_brains/boiler_brains.ino
+++ boiler_brains/boiler_brains.ino
@@ -446,6 +446,7 @@ String HandleAPI(String Header) { // Handle HTTP API calls (this ain't gonna be 
     if (StartupPercent < 10) StartupPercent = 10;
     if (StartupPercent > 100) StartupPercent = 100;
     SetMemory();
+    if (ActiveRun) PowerAdjust(StartupPercent);
     return jsonSuccess;
   } else if (Header.indexOf("/?data_3=") == 0) { // Set Fallback Power
     Header.remove(0,9);
